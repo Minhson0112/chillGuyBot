3b61ec467ac26bbbc5a744d9ff460ee642640c87
STONE ダオミンソン <son.d.m@stonesystem.co.jp>
2026-03-25 14:46:16 +0900

    update: lỗi bigint

--- bot/models/partner.py
+++ bot/models/partner.py
@@ -6,16 +6,16 @@ from bot.config.database import Base
 class Partner(Base):
     __tablename__ = "partner"
 
-    id = Column(BigInteger(unsigned=True), primary_key=True, autoincrement=True, nullable=False)
-    guild_id = Column(BigInteger(unsigned=True), unique=True, nullable=False)
+    id = Column(BigInteger, primary_key=True, autoincrement=True, nullable=False)
+    guild_id = Column(BigInteger, unique=True, nullable=False)
     guild_name = Column("guild_name", String(255), nullable=False)
     representative_user_id = Column(
-        BigInteger(unsigned=True),
+        BigInteger,
         ForeignKey("member.user_id"),
         nullable=False
     )
     partnered_by_user_id = Column(
-        BigInteger(unsigned=True),
+        BigInteger,
         ForeignKey("member.user_id"),
         nullable=False
     )
