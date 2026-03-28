db32e92f3cec5923c6bb1543e2f806c01a2440d7
DESKTOP-F8120DF\Minh_Son <kakashi01vl@gmail.com>
2026-03-29 02:13:51 +0900

    udpate thêm từ

--- bot/services/wordle/wordleDictionaryStartupService.py
+++ bot/services/wordle/wordleDictionaryStartupService.py
@@ -1,12 +1,19 @@
 from bot.config.database import getDbSession
 from bot.repository.wordGuessInputRepository import WordGuessInputRepository
+from bot.repository.wordRepository import WordRepository
 from bot.services.wordle.wordleDictionaryCacheService import wordleDictionaryCacheService
-
 
 class WordleDictionaryStartupService:
     def loadWordsToCache(self):
         with getDbSession() as session:
             wordGuessInputRepository = WordGuessInputRepository(session)
-            words = wordGuessInputRepository.findAllGuessWords()
-            wordleDictionaryCacheService.setWords(words)
+            wordRepository = WordRepository(session)
+
+            guessWords = wordGuessInputRepository.findAllGuessWords()
+            keyWords = wordRepository.findAllKeyWords()
+
+            allWords = guessWords + keyWords
+
+            wordleDictionaryCacheService.setWords(allWords)
             return wordleDictionaryCacheService.count()
+        
